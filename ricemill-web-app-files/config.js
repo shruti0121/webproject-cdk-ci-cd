8f27a22351ce678dc3a92f6893e36e379c2de292
shruti0121 <shruti0121@server.ca>
2026-07-25 23:19:52 -0400

    Added frontend files

---
+++ ricemill-web-app-files/config.js
@@ -0,0 +1,12 @@
+window.APP_CONFIG = {
+  region: "us-east-2",
+  cognito: {
+      userPoolId: "us-east-2_ZyqChxtMP",
+      clientId: "1pso9ba6p6o8gbaatjls2gh2sa"
+  },
+  api: {
+      baseUrl: "https://qymt8wl5rh.execute-api.us-east-2.amazonaws.com/prod/ricemill",
+      
+  },
+ 
+};
